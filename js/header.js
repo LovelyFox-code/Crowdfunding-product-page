acd5fc015bb534dbe7be2c403aa4bc7654a6d09a
Alina <alinadakhno60@gmail.com>
2022-04-28 12:34:46 +0200

    create burger menu

---
+++ js/header.js
@@ -0,0 +1,14 @@
+function burgerMenu() {
+    let links = document.getElementById("links");
+    let close = document.getElementById("close");
+    let open = document.getElementById("open");
+    if (links.style.display === "block") {
+        close.style.display = "none";
+        open.style.display = "block";
+        links.style.display = "none";
+    } else {
+        close.style.display = "block";
+        open.style.display = "none";
+        links.style.display = "block";
+    }
+  }
