5a536596bf4f12d42da76217e256befc5535bdf5
Alina <alinadakhno60@gmail.com>
2022-05-12 11:01:54 +0200

    burger menu open/close

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,26 +1,10 @@
 let openMenu = document.getElementsByClassName("open")[0];
-let closeMenu =document.getElementsByClassName("close")[0];
+let closeMenu =document.getElementsByClassName("close");
 let links = document.getElementsByClassName("links");
 let overlay = document.getElementsByClassName("overlay");
 
-window.onload = function()
-{
 openMenu.onclick = function() {
     links.style.display = "block";
     overlay.style.display = "block";
     console.log("clicked");
   }
-}
-function changeColor()
-{
-  links.style.display = "block";
-  overlay.style.display = "block";
-  console.log("clicked");
-}
-
-
-window.onload = function()
-{
-
-openMenu.onclick = changeColor;
-};
