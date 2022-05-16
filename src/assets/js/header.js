032530c2ac910d9b4fd43b62ba9671333adf027a
Alina <alinadakhno60@gmail.com>
2022-05-16 09:48:50 +0200

    adjustment

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,10 +1,20 @@
-let openMenu = document.getElementsByClassName("open")[0];
-let closeMenu =document.getElementsByClassName("close");
-let links = document.getElementsByClassName("links");
-let overlay = document.getElementsByClassName("overlay");
 
-openMenu.onclick = function() {
+let openMenu = document.getElementsByClassName("open")[0];
+let closeMenu = document.getElementsByClassName("close")[0];
+let links = document.getElementsByClassName("links")[0];
+let overlay = document.getElementsByClassName("overlay")[0];
+
+openMenu.onclick = function () {
     links.style.display = "block";
     overlay.style.display = "block";
+    closeMenu.style.display = "block";
+    openMenu.style.display = "none";
     console.log("clicked");
-  }
+}
+closeMenu.onclick = function () {
+
+    closeMenu.style.display = "none";
+    openMenu.style.display = "block";
+    links.style.display = "none";
+    overlay.style.display = "none";
+}
--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,40 +1,38 @@
 // Get the modal
-let modal = document.getElementsById("modal");
-
-// Get the button that opens the modal
+let modal = document.getElementsByClassName("modal")[0];
 let btn = document.getElementById("open-modal");
-
-// Get the <svg> element that closes the modal
 let closeBtn = document.getElementById("close");
-
-// When the user clicks the button, open the modal 
+//open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
-
+  overlay.style.display = "block";
 }
-
-// When the user clicks on <svg> (x), close the modal
 closeBtn.onclick = function() {
   modal.style.display = "none";
-
+  overlay.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
+// By clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    overlay.style.display = "none";
   }
 }
 
-//pledge open
-// let activeRadio = document.getElementsByClassName("radio__item");
-// let pledge = document.getElementsByClassName("selected-pledge");
-// let boxModal = document.getElementsByClassName("modal-content__box");
-// activeRadio.onclick = function() {
-//   pledge.style.display = "block";
-//   boxModal.style.border = "1px solid rgba(60, 179, 171, 1)"
-// }
-//bamboo plage open
+// pledge open
+let activeRadio = document.getElementsByClassName("radio__item");
+let pledge = document.getElementsByClassName("selected-pledge")[0];
+let boxModal = document.getElementsByClassName("modal__content__box");
+let modalContent = document.getElementsByClassName("modal__content");
+
+for(let i = 0; i<activeRadio.length; i++){
+  activeRadio[i].addEventListener('click', function handleClick(){
+    boxModal[i].classList.add("selected");
+    pledge.style.display = "block";
+  })
+}
+
+// bamboo plage open
 
 let continueBamboo = document.getElementById("bamboo");
 let successBamboo = document.getElementById("success");
@@ -43,6 +41,10 @@ continueBamboo.onclick = function() {
   successBamboo.style.display = "block";
   modal.style.display = "none";
 }
+continueBamboo.addEventListener('click', function handleClick(){
+  // successBamboo.classList.add("selected");
+  successBamboo.style.display = "block";
+})
 btnClose.onclick = function() {
   successBamboo.style.display = "none";
 }
