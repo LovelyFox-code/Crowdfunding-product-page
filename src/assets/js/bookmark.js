4ad15e90198b0e6ee7a09e437b37c57716438872
Alina <alinadakhno60@gmail.com>
2022-05-17 17:16:45 +0200

    start style bookmark btn by click

--- src/assets/js/bookmark.js
+++ src/assets/js/bookmark.js
@@ -1,2 +1,7 @@
 //button bookmark 
+let bookmark = document.getElementsByClassName("bookmark")[0];
 //on click change for bookmarked 
+bookmark.onclick = function(){
+    bookmark.innerHTML="bookmarked";
+    
+}
