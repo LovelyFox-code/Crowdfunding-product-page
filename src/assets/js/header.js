f7944e1d286ab2e4dd3c66f4f8d97222b3f54b90
Alina <alinadakhno60@gmail.com>
2022-05-17 14:12:29 +0200

    redacted js structure

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,20 +1,18 @@
-
 let openMenu = document.getElementsByClassName("open")[0];
 let closeMenu = document.getElementsByClassName("close")[0];
 let links = document.getElementsByClassName("links")[0];
 let overlay = document.getElementsByClassName("overlay")[0];
 
 openMenu.onclick = function () {
-    links.style.display = "block";
-    overlay.style.display = "block";
-    closeMenu.style.display = "block";
-    openMenu.style.display = "none";
-    console.log("clicked");
-}
+  links.style.display = "block";
+  overlay.style.display = "block";
+  closeMenu.style.display = "block";
+  openMenu.style.display = "none";
+  console.log("clicked");
+};
 closeMenu.onclick = function () {
-
-    closeMenu.style.display = "none";
-    openMenu.style.display = "block";
-    links.style.display = "none";
-    overlay.style.display = "none";
-}
+  closeMenu.style.display = "none";
+  openMenu.style.display = "block";
+  links.style.display = "none";
+  overlay.style.display = "none";
+};
