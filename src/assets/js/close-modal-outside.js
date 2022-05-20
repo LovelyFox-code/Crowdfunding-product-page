6aa1e0746f9b065588b5d86730b2b4734a11f7e3
Alina <alinadakhno60@gmail.com>
2022-05-20 15:04:53 +0200

    close modal by clicking outside the div(for modal and nav)

---
+++ src/assets/js/close-modal-outside.js
@@ -0,0 +1,10 @@
+// By clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == overlay) {
+      modal.style.display = "none";
+      overlay.style.display = "none";
+      closeMenu.style.display = "none";
+      openMenu.style.display = "block";
+      links.style.display = "none";
+    }
+  };
