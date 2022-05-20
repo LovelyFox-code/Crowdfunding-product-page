ae2f900ece4a31464a882eedb9cf633f3e2cbae0
Alina <alinadakhno60@gmail.com>
2022-05-20 15:03:27 +0200

    bookmark change style onclick

--- src/assets/js/bookmark.js
+++ src/assets/js/bookmark.js
@@ -1,7 +1,14 @@
 //button bookmark 
 let bookmark = document.getElementsByClassName("bookmark")[0];
 //on click change for bookmarked 
+
 bookmark.onclick = function(){
-    bookmark.innerHTML="bookmarked";
+    bookmark.classList.toggle("bookmark-active");
+    // bookmark.innerHTML="bookmarked";
     
 }
+// bookmark.addEventListener("click", function handleBookmark() {
+//     bookmark.classList.add("bookmark-active");
+//     bookmark.innerHTML="bookmarked";
+//     console.log("it works");
+//   });
