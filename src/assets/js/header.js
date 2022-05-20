d4fef1f2c9d9f973d5826b4672ef4b6033a1bbc2
Alina <alinadakhno60@gmail.com>
2022-05-20 15:05:43 +0200

    redact

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -8,6 +8,7 @@ openMenu.onclick = function () {
   overlay.style.display = "block";
   closeMenu.style.display = "block";
   openMenu.style.display = "none";
+  console.log("clicked");
 };
 closeMenu.onclick = function () {
   closeMenu.style.display = "none";
--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,20 +1,18 @@
 // Get the modal
 let modal = document.getElementsByClassName("modal")[0];
-let btn = document.getElementById("open-modal");
+let btn = document.getElementsByClassName("open-modal");
 let closeBtn = document.getElementById("close");
 //open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-  overlay.style.display = "block";
-};
+for(let i = 0; i < btn.length; i++){
+  btn[i].onclick = function () {
+    modal.style.display = "block";
+    overlay.style.display = "block";
+    window.scrollTo(0, 0);
+  };
+}
+//close modal
 closeBtn.onclick = function () {
   modal.style.display = "none";
   overlay.style.display = "none";
 };
-// By clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-    overlay.style.display = "none";
-  }
-};
+
