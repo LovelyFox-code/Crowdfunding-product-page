f1aca07ec07702cf8d3dd664bd01c0fb5dd81e73
Alina <alinadakhno60@gmail.com>
2022-05-20 15:06:11 +0200

    addPledge

--- src/assets/js/pledge.js
+++ src/assets/js/pledge.js
@@ -1,28 +1,23 @@
-
 // pledge open
 let activeRadio = document.getElementsByClassName("radio__item");
 let pledge = document.getElementsByClassName("selected-pledge");
 let boxModal = document.getElementsByClassName("modal__content__box");
 
-for (let i = 0; i < activeRadio.length; i++) {
-  activeRadio[i].addEventListener("click", function handleClick() {
-    boxModal[i].classList.add("selected");
-    pledge[i - 1].style.display = "block";
-  });
+for (let i = 0; i < boxModal.length; i++) {
+  boxModal[i].addEventListener("click", () =>
+    addPledge(boxModal[i], pledge[i])
+  );
 }
+function addPledge(div, btn) {
+  for (let i = 0; i < boxModal.length; i++) {
+    boxModal[i].classList.remove("selected");
+    pledge[i].style.display = "none";
+  }
+  if(div.classList.contains("disable")){
+    console.log("Disable");
+  }else{
+    div.classList.add("selected");
+    btn.style.display = "block";
+  }
 
-// bamboo plage open
-
-let continueBamboo = document.getElementById("bamboo");
-let successBamboo = document.getElementsByClassName("modal__success")[0];
-let btnClose = document.getElementById("btn-close");
-continueBamboo.onclick = function () {
-  modal.style.display = "none";
-  successBamboo.style.display = "block";
-  window.scrollTo(0, 0);
-  overlay.style.display = "block";
-};
-btnClose.onclick = function () {
-  successBamboo.style.display = "none";
-  overlay.style.display = "none";
-};
+}
